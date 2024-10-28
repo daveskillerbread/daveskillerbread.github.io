63d806bf79a2a05a9d6b22bb7bfe89dd0e0ebf6d
daveskillerbread <cartergriffin22@gmail.com>
2024-10-27 23:53:55 -0400

    changes changes changes
    added button to change background color randomly, added input text box and button to change text

--- script.js
+++ script.js
@@ -1,3 +1,35 @@
+let messages = [
+    "You clicked the button!",
+    "You clicked the button again!",
+    "Wow, you clicked the button again...",
+    "Stop clicking the fucking button asshat I don't get paid enough for this shit"
+];
+
+let clickCount = 0;
+
+
+
+
 function changeText() {
-    document.getElementById("greeting").textContent = "You clicked the button!";
+    if (clickCount < messages.length){
+    document.getElementById("greeting").textContent = messages[clickCount];
+    clickCount++;
+    } else{
+        console.log("No more text changes available.")
+    }
+}
+
+function changeBackground(){
+    const randomColor = '#' + Math.floor(Math.random()*16777215).toString(16);
+    document.body.style.backgroundColor = randomColor;
+}
+
+function displayGreeting(){
+    const name = document.getElementById("nameInput").value;
+
+    if(name){
+        document.getElementById("greeting").textContent = `Hello, ${name}! Welcome to Dave's Killer Webpage!`;
+    } else{
+        document.getElementById("greeting").textContent = "Please enter your name!";
+    }
 }
